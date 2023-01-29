aa8cbb01b5fc23940e9ea0b16d47c5c85702ad55
Asbjørn <asbjorn454@gmail.com>
2023-01-29 01:16:18 +0100

    Added PVP function, and hover function on button

--- javascript.js
+++ javascript.js
@@ -7,8 +7,12 @@ function getComputerChoice(){
 }
 
 let computerSelection
+let currentPlayer = 1;
 let playerSelection
+let player1Selection
+let player2Selection
 let playerScore = 0;
+let player2Score = 0;
 let computerScore = 0;
 //result = 0, player wins
 //result = 1, computer wins
@@ -49,6 +53,50 @@ function playRound(playerSelection, computerSelection){
         document.getElementById('scissorsButton').style.display="none"
         document.getElementById('restartButton').style.display="block"
         document.getElementById('endMessage').innerHTML = 'The game is over, computer won. If you wish to play again, press the restart button'
+    }}
+
+function getPlayerSelections(playerSelection){
+    if(currentPlayer == 1){
+    player1Selection = playerSelection;
+    console.log("player1 picked " + player1Selection)
+    currentPlayer +=1;
+    }else if(currentPlayer == 2){
+        player2Selection = playerSelection;
+        console.log("player2 picked " + player2Selection)
+        playPVPRound();
+        currentPlayer= 1;
+    }
+}
+function playPVPRound(){
+    if(player1Selection === "rock" && player2Selection === "rock" || player1Selection === "scissors" && player2Selection === "scissors" || player1Selection === "paper" && player2Selection === "paper"){
+        console.log("You both picked " + player2Selection + ", its a tie!")
+    } else if ( player1Selection === "rock" && player2Selection === "scissors" || player1Selection === "paper" && player2Selection === "rock" || player1Selection === "scissors" && player2Selection === "paper"){
+        console.log("Player picked " + player1Selection + " and won")
+        playerScore += 1;
+    }else if (player1Selection === "rock" && player2Selection === "paper" || player1Selection === "paper" && player2Selection === "scissors" || player1Selection === "scissors" && player2Selection === "rock"){
+        console.log("Player2 picked " + player2Selection + " and won")
+        player2Score +=1;
+    }
+    else {
+        console.log("Input did not match any expected input")
+
+    }
+    document.getElementById('computerAndPlayerPicks').innerHTML = 'Player1 picked ' + player1Selection + " | Player2 picked " + player2Selection
+    document.getElementById('playerScoreHTML').innerHTML = 'Player1 score = ' + playerScore;
+    document.getElementById('computerScoreHTML').innerHTML = 'Player2 score = ' + player2Score;
+
+    if(playerScore === 3){
+        document.getElementById('rockButton').style.display="none"
+        document.getElementById('paperButton').style.display="none"
+        document.getElementById('scissorsButton').style.display="none"
+        document.getElementById('restartButton').style.display="block"
+        document.getElementById('endMessage').innerHTML = 'The game is over, player1 won. If you wish to play again, press the restart button'
+    } else if (player2Score === 3){
+        document.getElementById('rockButton').style.display="none"
+        document.getElementById('paperButton').style.display="none"
+        document.getElementById('scissorsButton').style.display="none"
+        document.getElementById('restartButton').style.display="block"
+        document.getElementById('endMessage').innerHTML = 'The game is over, player2 won. If you wish to play again, press the restart button'
     }}
     function restartGame(){
         document.location.reload();
